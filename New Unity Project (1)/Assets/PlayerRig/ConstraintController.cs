de2000abb131e7fc9b4397d0661afda22cad7406
RetroSpecter <InsertNameHere4897@gmail.com>
2020-04-18 00:56:46 -0700

    refined ik system and added movement

---
+++ New Unity Project (1)/Assets/PlayerRig/ConstraintController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations.Rigging;
+
+public class ConstraintController : MonoBehaviour
+{
+
+    MultiPositionConstraint mps;
+    MultiRotationConstraint mrs;
+    public Transform target;
+
+    void Start()
+    {
+        mps = GetComponent<MultiPositionConstraint>();
+        mrs = GetComponent<MultiRotationConstraint>();
+    }
+
+    void Update()
+    {
+        mps.data.offset = target.transform.localPosition;
+        mrs.data.offset = target.transform.localEulerAngles;
+    }
+}
---
+++ New Unity Project (1)/Assets/PlayerRig/TorsoAnimationController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorsoAnimationController : MonoBehaviour
+{
+
+    [SerializeField] LegStepper leftLeg;
+    [SerializeField] LegStepper rightLeg;
+    [SerializeField] Transform leftHomePosition;
+    [SerializeField] Transform rightHomePosition;
+    [SerializeField]  LayerMask ground;
+
+    void Awake()
+    {
+        StartCoroutine(LegUpdateCoroutine());
+    }
+
+    private void Update()
+    {
+        FloorTransform(leftHomePosition);
+        FloorTransform(rightHomePosition);
+    }
+
+    public void FloorTransform(Transform home)
+    {
+        Vector3 raycastPos = home.position;
+        raycastPos.y = transform.position.y;
+        Debug.DrawRay(raycastPos, -transform.up * 10, Color.blue);
+
+        RaycastHit hit;
+        if (Physics.Raycast(raycastPos, -transform.up, out hit, 1000, ground))
+        {
+            home.position = hit.point;
+        }
+    }
+
+    IEnumerator LegUpdateCoroutine()
+    {
+        // Run continuously
+        while (true)
+        {
+            // Try moving one diagonal pair of legs
+            do
+            {
+                leftLeg.TryMove();
+                yield return null;
+            } while (leftLeg.Moving);
+
+            do
+            {
+                rightLeg.TryMove();
+                yield return null;
+            } while (rightLeg.Moving);
+        }
+    }
+
+}
