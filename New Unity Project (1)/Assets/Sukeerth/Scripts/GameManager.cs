573b0f5e112939e8666c5f21100c87f164542f68
Sukeerth <ssvegaraju@gmail.com>
2020-04-18 12:16:48 -0700

    Starting work on stage progression, placed some outlets

---
+++ New Unity Project (1)/Assets/Sukeerth/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    #region Singleton
+    public static GameManager instance;
+    private void Awake() {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
+    }
+    #endregion
+    public Stage currentStage {
+        get;
+        private set;
+    }
+    public event Action<Stage> OnStageChange;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+public enum Stage
+{
+    Stage1, Stage2, Stage3
+}
---
+++ New Unity Project (1)/Assets/Sukeerth/Scripts/OutletManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutletManager : MonoBehaviour
+{
+    #region Singleton
+    public static OutletManager instance;
+    private void Awake() {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
+    }
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public int GetOutlets(Stage currentStage) {
+        return 5;
+    }
+}
