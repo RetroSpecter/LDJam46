27dc3ce63734c71fd2d3db4dcae8d05078ace780
Sukeerth <ssvegaraju@gmail.com>
2020-04-18 22:24:12 -0700

    Starting work on main scene

--- New Unity Project (1)/Assets/Sukeerth/Menu/MenuManager.cs
+++ New Unity Project (1)/Assets/Sukeerth/Menu/MenuManager.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class MenuManager : MonoBehaviour
 {
+    public float inputDelay = 0.2f;
     public MenuButton[] buttons;
+
+    private int index;
+    private float lastSelectTime;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +19,21 @@ public class MenuManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Time.time - lastSelectTime < inputDelay) {
+            return;
+        }
+        if (Input.GetButtonDown("Submit")) {
+            buttons[index].OnPress();
+            return;
+        }
+        float input = Input.GetAxis("Vertical");
+        if (input == 0) {
+            return;
+        }
+        lastSelectTime = Time.time;
+        buttons[index].OnExitHover();
+        index += (int)Mathf.Sign(input);
+        index = Mathf.Clamp(index, 0, buttons.Length - 1);
+        buttons[index].OnHover();
     }
 }
